6e993fc73c75cb7796c2379f235af4ba570c9a93
Brian Bycynski <high5bri@hotmail.com>
2021-11-06 04:35:14 -0400

    working on React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-//
-//
-
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
@@ -13,20 +10,16 @@ function App() {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        {
-          name: uName,
-          age: uAge,
-          id: Math.random().toString(),
-        },
+        { name: uName, age: uAge, id: Math.random().toString() },
       ];
     });
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,22 +1,17 @@
-//
-//
-
 import React from 'react';
 
-import classes from '../../css/UsersList.module.css';
 import Card from '../UI/Card';
+import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map((user) => {
-          return (
-            <li key={user.id}>
-              {user.name} ({user.age} years old)
-            </li>
-          );
-        })}
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
       </ul>
     </Card>
   );
